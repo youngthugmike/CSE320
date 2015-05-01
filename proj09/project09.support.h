c8f6c9291b6c2a4e659b93b333a3bf70b37ebb9c
chenxi28 <chenxi28@msu.edu>
2015-05-01 10:36:57 -0400

    789

---
+++ proj09/project09.support.h
@@ -0,0 +1,13 @@
+
+int negate( int N );
+int absolute( int N );
+
+int add( int A, int B );
+int sub( int A, int B );
+int mul( int A, int B );
+int divide( int A, int B );
+int remain( int A, int B );
+
+int power( int N, int I );
+int factorial( int N );
+
